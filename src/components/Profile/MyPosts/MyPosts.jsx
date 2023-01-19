db03b2374b13697bd7cd0dba7cfc199e5e87151e
sdubouski <sergeidubovskiy1990@gmail.com>
2023-01-20 00:47:04 +0300

    less23

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import s from'./MyPosts.module.css';
+import s from './MyPosts.module.css';
 import {Post} from './Post/Post';
 
 export const MyPosts = () => {
     return (
-      <div>
-        My post
-        <div>
-          <textarea></textarea>
-          <button> Add post</button>
-        </div>
-        <div className={s.posts}>
-          <Post message = 'Hi loh' likesCount='23'/>
-          <Post message = 'it is my first post' likesCount='20'/>
+        <h3 className='postsBlock'>
+            My post
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <button> Add post</button>
+            </div>
+            <div className={s.posts}>
+                <Post message='Hi loh' likesCount='23'/>
+                <Post message='it is my first post' likesCount='20'/>
 
-        </div>
-      </div>
+            </div>
+        </h3>
     );
 }
 
